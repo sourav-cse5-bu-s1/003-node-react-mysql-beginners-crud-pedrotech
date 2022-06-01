56286be3f3aee6dcb2c4b87e400d69af45b8e957
Sourav Malo <sourav.cse5.bu.s1@gmail.com>
2022-06-01 18:26:54 +0600

    create employee done

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [name, setName] = useState('')
+  const [age, setAge] = useState(0)
+  const [position, setPosition] = useState('')
+  const [country, setCountry] = useState('')
+  const [wage, setWage] = useState(0)
+
+  const addEmployee = () => {
+    axios.post(
+      'http://localhost:3001/create', 
+      { name, age, position, country, wage }
+    )
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='information'>
+        <label>Name</label>
+        <input type='text' onChange={(e) => setName(e.target.value)}/>
+        <label>Age</label>
+        <input type='number' onChange={(e) => setAge(e.target.value)}/>
+        <label>Position</label>
+        <input type='text' onChange={(e) => setPosition(e.target.value)}/>
+        <label>Country</label>
+        <input type='text' onChange={(e) => setCountry(e.target.value)}/>
+        <label>Wage (year)</label>
+        <input type='number' onChange={(e) => setWage(e.target.value)}/>
+        <button onClick={addEmployee}>Add Employee</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
